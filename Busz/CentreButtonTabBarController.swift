afe2742f26e00f1f6f8863bf3a2bf35545fe804e
Hairui Lin <hairuilin@hairuis-macbook-air.local>
2017-09-25 00:12:51 +0800

    Configured the Custom Tabbar to display middle button correctly.

--- Busz/CentreButtonTabBarController.swift
+++ Busz/CentreButtonTabBarController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class CentreButtonTabBarController: UITabBarController {
 
-    
+    private let imageDimension : CGFloat = 70
     private let heightOfTabbar : CGFloat = 49
+
     override func viewDidLoad() {
         super.viewDidLoad()
         addMiddleButton()
     }
     
     private func addMiddleButton(){
-        let customButton = UIButton(type: .custom)
-        customButton.backgroundColor = .red
-        self.view.addSubview(customButton)
         
-        customButton.translatesAutoresizingMaskIntoConstraints = false
-        customButton.centerXAnchor.constraint(equalTo: self.tabBar.centerXAnchor, constant: 0).isActive = true
-        customButton.centerYAnchor.constraint(equalTo: self.tabBar.centerYAnchor, constant: 0).isActive = true
-        customButton.heightAnchor.constraint(equalToConstant: heightOfTabbar).isActive = true
-        customButton.widthAnchor.constraint(equalToConstant: heightOfTabbar).isActive = true
+        //add image view
+        let iconImage = UIImage(named: "busButtonIcon")
+        let imageView = UIImageView(image: iconImage)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = imageDimension / 2
+        self.tabBar.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: self.tabBar.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: imageDimension).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: imageDimension).isActive = true
+        let bottomPadding = heightOfTabbar - (imageDimension/2)
+        imageView.bottomAnchor.constraint(equalTo: self.tabBar.bottomAnchor, constant: -bottomPadding).isActive = true
+        
     }
 
 }
