0a7cc28bebb2f80a3d97b9305401701e5f35647b
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-27 00:25:11 +0800

    Updated Strings file.

--- Busz/Strings.swift
+++ Busz/Strings.swift
@@ -13,4 +13,5 @@ struct Strings {
     static let kError = "Error"
     static let kAllowLocationAccess = "Please allow app the app to always acess location on your device"
     static let kGeoFencingFeatureNotAvailable = "Geofencing feature is not supported on this device"
+    static let kAllowNotificationAccess = "Please enable Notification for Busz for the device to receive notifications"
 }
