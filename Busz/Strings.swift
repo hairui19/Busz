d02e5ca707cf08046767f645f3ec0482cafd4bba
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-27 01:34:25 +0800

    updated Strings file.

--- Busz/Strings.swift
+++ Busz/Strings.swift
@@ -11,6 +11,8 @@ import Foundation
 struct Strings {
     static let kWarning = "Warning"
     static let kError = "Error"
+    static let kAttention = "Attention"
+    static let kAlmostArrived = "You have almost arrived at your place!"
     static let kAllowLocationAccess = "Please allow app the app to always acess location on your device"
     static let kGeoFencingFeatureNotAvailable = "Geofencing feature is not supported on this device"
     static let kAllowNotificationAccess = "Please enable Notification for Busz for the device to receive notifications"
