0cd957b0df77699f2c2a9971863d92e23c387833
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-27 00:21:05 +0800

    Added identifiers struct to contain all identifier strings.

---
+++ Busz/Identifiers.swift
@@ -0,0 +1,16 @@
+//
+//  Identifiers.swift
+//  Busz
+//
+//  Created by Hairui Lin on 27/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+
+struct Identifiers {
+    static let kNormalBusStopAnnotationView = "NormalBusAnnotationView"
+    static let kDestinationBusStopAnnotationView = "DestinationBusStopAnnotationView"
+    static let kMonitoringRegion = "MonitoringRegion"
+    
+}
