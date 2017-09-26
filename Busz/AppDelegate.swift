3e827278753e94b7f076613b8470e4f2c88d1a84
Hairui Lin <hairuilin@hairuis-macbook-air.local>
2017-09-27 00:24:40 +0800

    Configured the app to receive local notifications.

--- Busz/AppDelegate.swift
+++ Busz/AppDelegate.swift
@@ -8,40 +8,52 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegate{
-
+class AppDelegate: UIResponder, UIApplicationDelegate{
+    
     var window: UIWindow?
     let locationManager = CLLocationManager()
+    let notificationCenter = UNUserNotificationCenter.current()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // setup location mananger
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
+        
+        // setup local notification.
+        notificationCenter.removeAllPendingNotificationRequests()
+        notificationCenter.delegate = self
+        setupGeofencingNotification()
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
+
+extension AppDelegate: CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
+    func setupGeofencingNotification(){
+        notificationCenter.getNotificationSettings {[weak self] (notificationSetting) in
+            let authorisationStatus = notificationSetting.authorizationStatus
+            if authorisationStatus == .notDetermined {
+                self?.notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+                    if success {
+                        print("this is success")
+                    }
+                }
+            }else if authorisationStatus == .authorized{
+                //self?.scheduleLocationNotification()
+            }else if authorisationStatus == .denied{
+                Utility.showAlert(in: (self!.window?.rootViewController)!, title: Strings.kError, message:Strings.kAllowNotificationAccess )
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .badge,.alert])
+    }
+    
+
+    
+}
+
 
