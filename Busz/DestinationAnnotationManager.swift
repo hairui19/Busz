a8b88e6698f6ec81e73b95a62cab19030e788fe7
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-26 14:10:37 +0800

    A strut to manage the display of destination annotation in map view.

---
+++ Busz/DestinationAnnotationManager.swift
@@ -0,0 +1,27 @@
+//
+//  DestinationAnnotationManager.swift
+//  Busz
+//
+//  Created by Hairui Lin on 26/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+
+
+struct DestinationAnnotationManager {
+    var previousDestionationAnnotation : DestinationBusStopAnnotation?
+    var currentDestionationAnnotation : DestinationBusStopAnnotation?
+    
+    mutating func update( _ destinationAnnotaion : DestinationBusStopAnnotation){
+        previousDestionationAnnotation = currentDestionationAnnotation
+        currentDestionationAnnotation = destinationAnnotaion
+    }
+    
+    mutating func remove(){
+        previousDestionationAnnotation = currentDestionationAnnotation
+        currentDestionationAnnotation = nil
+    }
+    
+    init(){}
+}
