abb4e64f099115140710ba921736d87759e16c9c
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-26 09:32:49 +0800

    Added BusStops model.

---
+++ Busz/BusStops.swift
@@ -0,0 +1,15 @@
+//
+//  BusStops.swift
+//  Busz
+//
+//  Created by Hairui Lin on 26/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+
+
+struct BusStops {
+    let busStops : [BusStop]
+    let destinationBusStop : BusStop?
+}
