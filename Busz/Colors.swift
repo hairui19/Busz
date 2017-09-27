48816e2f235d3f5c5449c800dcfe76a9f052cb2c
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-28 01:43:01 +0800

    Updated Colors file.

--- Busz/Colors.swift
+++ Busz/Colors.swift
@@ -18,6 +18,8 @@ struct Colors {
     static let mediumPink = UIColor(r: 255, g: 96, b: 119, alpha: 1)
     static let mediumPuprple = UIColor(r: 207, g: 97, b: 131, alpha: 1)
     static let heavyPurple = UIColor(r: 115, g: 87, b: 127, alpha: 1)
+    static let lightYellow = UIColor(r: 248, g: 177, b: 149, alpha: 1)
+    static let heavyBlue = UIColor(r: 24, g: 59, b: 89, alpha: 1)
 }
 
 
