9722a4a3b36b5ac3bd83cab0a64947a47b520441
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-27 21:46:13 +0800

    Added a date extension to calculate the difference between arrival time and current time in minutes.

---
+++ Busz/Date+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Date+Extension.swift
+//  Busz
+//
+//  Created by Hairui Lin on 27/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+
+
+extension Date {
+    func offsetFrom(date: Date) -> String {
+        
+        let dayHourMinuteSecond: Set<Calendar.Component> = [.day, .hour, .minute, .second]
+        let difference = NSCalendar.current.dateComponents(dayHourMinuteSecond, from: date, to: self);
+        
+        //let seconds = "\(difference.second ?? 0)s"
+        let minutes = "\(difference.minute ?? 0)m"
+        let hours = "\(difference.hour ?? 0)h" + " " + minutes
+        let days = "\(difference.day ?? 0)d" + " " + hours
+        
+        if let day = difference.day, day          > 0 { return days }
+        if let hour = difference.hour, hour       > 0 { return hours }
+        if let minute = difference.minute, minute > 0 { return minutes }
+        //if let second = difference.second, second > 0 { return seconds }
+        return ""
+    }
+    
+}
