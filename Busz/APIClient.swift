4d1eac58222613f09aba13a0a515774a778a69ac
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-27 21:43:54 +0800

    Update APIClient to read data for bus arrival.

--- Busz/APIClient.swift
+++ Busz/APIClient.swift
@@ -21,21 +21,33 @@ class APIClient {
     private let busRouteEndpoint = "BusRoutes"
     private let busServicesEndpoint = "BusServices"
     private let busStopsEndpoint = "BusStops"
+    private let busArrivalTimeEndpoint = "BusArrivalv2"
     
     
     // Errors
     private enum APIError : Error{
         case invalidURL(String)
         case invalidJSON(String)
+        case invalidParams(String, Any)
     }
     
-    private func request(endpoint : String) -> Observable<[String : Any]>{
+    private func request(endpoint : String, params : [String : Any] = [:]) -> Observable<[String : Any]>{
         do{
-            guard let url = URL(string: baseURL)?.appendingPathComponent(endpoint) else{
+            guard let url = URL(string: baseURL)?.appendingPathComponent(endpoint),
+                var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+            else{
                 throw APIError.invalidURL(endpoint)
             }
             
-            var request = URLRequest(url: url)
+            urlComponents.queryItems = try params.map({ (key, value) -> URLQueryItem in
+                guard let v = value as? CustomStringConvertible else{
+                    throw APIError.invalidParams(key, value)
+                }
+                return URLQueryItem(name: key, value: v.description)
+            })
+            
+            let finalURL = urlComponents.url!
+            var request = URLRequest(url: finalURL)
             request.setValue("application/json", forHTTPHeaderField: "Accept")
             request.addValue("lNquaoFlRWG4yXRGQNXYhg==", forHTTPHeaderField: "AccountKey")
             request.httpMethod = "GET"
@@ -47,7 +59,6 @@ class APIClient {
                 guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),let result = jsonObject as? [String : Any] else{
                     throw APIError.invalidJSON(url.absoluteString)
                 }
-                print("everything ok")
                 return result
             })
             
@@ -57,7 +68,36 @@ class APIClient {
         }
     }
     
-    func busRoutes()-> Observable<[String : Any]>{
-        return request(endpoint: busStopsEndpoint)
+    func getBusArrivalTime(busStopCode : String, serviceNo : String, busStopName : String) -> Observable<String?>{
+        let params = [
+            "BusStopCode" : busStopCode,
+            "ServiceNo" : serviceNo
+        ]
+        return request(endpoint: busArrivalTimeEndpoint, params: params)
+            .map({ dataDic -> String? in
+                guard let services = dataDic["Services"] as? [Any],
+                    services.count > 0,
+                    let nextBus = services[0] as? [String : Any],
+                let nextBusInfo = nextBus["NextBus"] as? [String : Any],
+                let estimatedArrival = nextBusInfo["EstimatedArrival"] as? String else{
+                        return nil
+                }
+                let arrivalTime = estimatedArrival
+                let dateFormatter = ISO8601DateFormatter()
+                let date: Date? = dateFormatter.date(from: arrivalTime)
+                if let theArrivingDate = date{
+                    let arrivingTime = theArrivingDate.offsetFrom(date: Date())
+                    if arrivingTime == ""{
+                        return "Bus \(serviceNo) has arrived at \(busStopName)"
+                    }else{
+                        return "Bus \(serviceNo) is arriving at \(busStopName) in \(arrivingTime)"
+                    }
+                }else{
+                    return nil
+                }
+                
+            })
+        
     }
 }
+
