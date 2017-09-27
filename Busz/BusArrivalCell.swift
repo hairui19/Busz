2fe9e52cf48b42219410d72b8ef54447facd2a00
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-28 01:21:59 +0800

    Update busArrivalCell for UIs.

--- Busz/BusArrivalCell.swift
+++ Busz/BusArrivalCell.swift
@@ -12,17 +12,22 @@ class BusArrivalCell: UICollectionViewCell {
     @IBOutlet weak var busNumberLabel: UILabel!
     @IBOutlet weak var busStopNameLabel: UILabel!
     @IBOutlet weak var estArrivalLabel: UILabel!
+    @IBOutlet weak var bustCodeLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        busNumberLabel.textColor = Colors.heavyPurple
+        busStopNameLabel.textColor = Colors.heavyPurple
+        estArrivalLabel.textColor = Colors.heavyPurple
+        bustCodeLabel.textColor = Colors.heavyPurple
         self.backgroundColor = .white
         self.layer.cornerRadius = 10
         self.layer.shadowRadius = 5
         self.layer.shadowColor = UIColor.lightGray.cgColor
         self.layer.shadowOpacity = 1
         self.layer.shadowOffset = CGSize(width: 1, height: 1)
-        self.layer.shadowColor = Colors.mediumPuprple.cgColor
+        self.layer.shadowColor = Colors.heavyPurple.cgColor
         self.clipsToBounds = false
         self.layer.masksToBounds = false
     }
