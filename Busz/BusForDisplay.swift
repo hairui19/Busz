db6f9a257532ecee1df72488f193d89624da87e8
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-28 13:09:37 +0800

    Added a model for displaying of bus information in mainviewcontroller.

---
+++ Busz/BusForDisplay.swift
@@ -0,0 +1,16 @@
+//
+//  BusForDisplay.swift
+//  Busz
+//
+//  Created by Hairui Lin on 28/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+
+
+struct BusForDisplay {
+    let busNumber : String
+    let busStopCode : String
+    let busStopName : String
+}
