a78db7e7fb8ba6372433a6422fa6c7fdec47b08f
Hairui Lin <hairuilin@hairuis-air.home>
2017-09-28 13:34:32 +0800

    Updated the cell to be in MVVM format.

--- Busz/BusArrivalCell.swift
+++ Busz/BusArrivalCell.swift
@@ -7,20 +7,47 @@
 //
 
 import UIKit
+import RxSwift
 
 class BusArrivalCell: UICollectionViewCell {
+    let apiClient = APIClient.share
+    let disposeBag = DisposeBag()
     @IBOutlet weak var busNumberLabel: UILabel!
     @IBOutlet weak var busStopNameLabel: UILabel!
     @IBOutlet weak var estArrivalLabel: UILabel!
-    @IBOutlet weak var bustCodeLabel: UILabel!
+    @IBOutlet weak var busCodeLabel: UILabel!
 
+    
+    var busForDisplay : BusForDisplay?{
+        didSet{
+            busNumberLabel.text = busForDisplay?.busNumber
+            busStopNameLabel.text = busForDisplay?.busStopName
+            busCodeLabel.text = busForDisplay?.busStopCode
+            apiClient.getBusArrivalTimeForDisplay(busStopCode: (busForDisplay?.busStopCode)!, serviceNo: (busForDisplay?.busNumber)!, busStopName: (busForDisplay?.busStopName)!)
+            .asObservable()
+            .debug()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] estimatedTimeMessage in
+                if let estimatedTimeMessage = estimatedTimeMessage {
+                    if estimatedTimeMessage == ""{
+                        self?.estArrivalLabel.text = "Est Arrival: Arrived"
+                    }else{
+                        self?.estArrivalLabel.text = "Est Arrival: \(estimatedTimeMessage)"
+                    }
+                }else{
+                    self?.estArrivalLabel.text = "No Data Avail"
+                }
+            })
+            .addDisposableTo(disposeBag)
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         busNumberLabel.textColor = Colors.heavyPurple
         busStopNameLabel.textColor = Colors.heavyPurple
         estArrivalLabel.textColor = Colors.heavyPurple
-        bustCodeLabel.textColor = Colors.heavyPurple
+        busCodeLabel.textColor = Colors.heavyPurple
         self.backgroundColor = .white
         self.layer.cornerRadius = 10
         self.layer.shadowRadius = 5
