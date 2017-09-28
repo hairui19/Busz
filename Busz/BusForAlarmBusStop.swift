4455b1d743d14d1d8a26b4c4e7918bdf7b919842
Hairui Lin <hairuilin@hairuis-macbook-air.local>
2017-09-28 17:10:02 +0800

    Added BusForAlarmBusStop realm model.

---
+++ Busz/BusForAlarmBusStop.swift
@@ -0,0 +1,19 @@
+//
+//  BusForAlarmBusStop.swift
+//  Busz
+//
+//  Created by Hairui Lin on 28/9/17.
+//  Copyright © 2017 Hairui Lin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BusForAlarmBusStop: Object {
+    dynamic var busNumber = ""
+    dynamic var busStopCode = ""
+    dynamic var busStopName = ""
+    dynamic var latitude : Double = 0.0
+    dynamic var longtitude : Double = 0.0
+    dynamic var timeStamp : Double = 0.0
+}
